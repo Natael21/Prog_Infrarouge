4f7d1fc6f9b46dcd3cc4948838399edbc0058577
Natael <nataellaroche@hotmail.com>
2020-10-29 16:27:56 -0400

    Changer
    dr

--- Infrarouge/src/main.cpp
+++ Infrarouge/src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
+#include <LibRobUS.h>
 
 void setup() {
   // put your setup code here, to run once:
+  BoardInit();
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(ROBUS_ReadIR(0) <= 680 && ROBUS_ReadIR(0) >= 180)
+  {
+    Serial.println("Objet !");
+  }
+  else
+  {
+    Serial.println("Rien !");
+  }
 }
